db20f7b058af348fcd14142c9fef2696c87bd126
Luis-devs <luismonteria2001@gmail.com>
2024-09-13 12:11:48 -0500

    Modificación para que se guarden los eventos que se puedan guardar

--- src/evento/evento.service.ts
+++ src/evento/evento.service.ts
@@ -52,11 +52,12 @@ export class EventoService {
       .find({ $or: condicionesConsulta })
       .exec();
 
+    const mensajeConflict = [];
+    const indexEventosConflicto = [];
     if (eventosEncontrados.length > 0) {
-      const mensajeConflict = [];
       eventosEncontrados.map((eventoInstructor) => {
         eventoInstructor.eventos.map((eventoReportado) => {
-          evento.eventos.map((eventosAReportar) => {
+          evento.eventos.map((eventosAReportar, indexEventoAReportar) => {
             let diaConnflicto = [];
             let repiteDia: boolean;
             eventoReportado.diastrabajados.map((diaEspecifico) => {
@@ -72,7 +73,7 @@ export class EventoService {
               repiteDia
             ) {
               mensajeConflict.push({
-                evento: eventoReportado,
+                evento: { ...eventoReportado, mes: eventosAReportar.mes },
                 mensaje: `Ya existe un evento en el ambiente ${
                   eventoReportado.ambiente.ambiente
                 } con horario ${
@@ -81,141 +82,163 @@ export class EventoService {
                   eventosAReportar.mes
                 } de ${eventosAReportar.year}`,
               });
+              if (!indexEventosConflicto.includes(indexEventoAReportar)) {
+                indexEventosConflicto.push(indexEventoAReportar);
+              }
             }
           });
         });
       });
-      if (mensajeConflict.length > 0) {
-        throw new ConflictException(mensajeConflict);
-      }
+    }
+    //Verifico si hay eventos en conflicto y los saco para crear los que están bien
+    if (mensajeConflict.length > 0) {
+      console.log(mensajeConflict);
+
+      const copia_evento = { ...evento };
+      copia_evento.eventos = evento.eventos.filter(
+        (evento, indexEvento) => !indexEventosConflicto.includes(indexEvento),
+      );
+      evento = { ...copia_evento };
     }
 
-    /**
-     * Valida los tiempos que se piensan agregar al gestor de tiempo
-     * y retorna un error en la respuesta en caso de encontrar
-     * alguno.
-     */
+    let registrosEventoExistente = [];
+    let eventosCrear = [];
+    let respGestor: any;
+    if (evento.eventos.length > 0) {
+      /**
+       * Valida los tiempos que se piensan agregar al gestor de tiempo
+       * y retorna un error en la respuesta en caso de encontrar
+       * alguno.
+       */
 
-    await this.validarTiempos(evento);
+      await this.validarTiempos(evento);
 
-    /**
-     * LLamada a los servicios que actualizan los gestores
-     */
-    const respGestor = await this.gestorTService.actualizarTiempos(evento);
+      /**
+       * LLamada a los servicios que actualizan los gestores
+       */
+      respGestor = await this.gestorTService.actualizarTiempos(evento);
 
-    //  Actualizar El ambiente
-    await this.gestorAmbienteService.actualizarAmbiente(
-      evento.eventos,
-      evento.instructor,
-    );
-
-    //Actualizar el gestor de la ficha
-    //['2684','2684','123']
-    let codigos = evento.eventos.flatMap((evento) => evento.ficha.codigo);
-    codigos = [...new Set(codigos)];
-    //[{codigo_ficha, meses:[{mes:1, horas:2}]}]
-    const reporteFichas: ReporteMesesDto[] = [];
-    codigos.forEach((cod) => {
-      const indexReporte = reporteFichas.findIndex(
-        (r) => r.codigo_ficha == cod,
+      //  Actualizar El ambiente
+      await this.gestorAmbienteService.actualizarAmbiente(
+        evento.eventos,
+        evento.instructor,
       );
-      //No se ha guardado el reporte de horas para la ficha
-      if (indexReporte == -1) {
-        const eventosCod = evento.eventos.filter(
-          (ev) => ev.ficha.codigo == cod,
-        );
-        if (eventosCod.length > 0) {
-          //{codigo_ficha, meses[]{mes:1, horas: 12}}
-          const repor: ReporteMesesDto = { codigo_ficha: cod, meses: [] };
-          eventosCod.forEach((ev) => {
-            const indexMes = repor.meses.findIndex((mes) => mes.mes == ev.mes);
-            indexMes == -1
-              ? repor.meses.push({ mes: ev.mes, horas: ev.horas })
-              : (repor.meses[indexMes].horas += ev.horas);
-          });
-          reporteFichas.push(repor);
-        }
-      }
-    });
 
-    /* {
+      //Actualizar el gestor de la ficha
+      //['2684','2684','123']
+      let codigos = evento.eventos.flatMap((evento) => evento.ficha.codigo);
+      codigos = [...new Set(codigos)];
+      //[{codigo_ficha, meses:[{mes:1, horas:2}]}]
+      const reporteFichas: ReporteMesesDto[] = [];
+      codigos.forEach((cod) => {
+        const indexReporte = reporteFichas.findIndex(
+          (r) => r.codigo_ficha == cod,
+        );
+        //No se ha guardado el reporte de horas para la ficha
+        if (indexReporte == -1) {
+          const eventosCod = evento.eventos.filter(
+            (ev) => ev.ficha.codigo == cod,
+          );
+          if (eventosCod.length > 0) {
+            //{codigo_ficha, meses[]{mes:1, horas: 12}}
+            const repor: ReporteMesesDto = { codigo_ficha: cod, meses: [] };
+            eventosCod.forEach((ev) => {
+              const indexMes = repor.meses.findIndex(
+                (mes) => mes.mes == ev.mes,
+              );
+              indexMes == -1
+                ? repor.meses.push({ mes: ev.mes, horas: ev.horas })
+                : (repor.meses[indexMes].horas += ev.horas);
+            });
+            reporteFichas.push(repor);
+          }
+        }
+      });
+
+      /* {
       reporte: [{ codigo_ficha: 123, meses: [{ mes: 1, horas: 120 }] }];
       year: 2022;
       id_instructor: '2684';
     } */
-    const now = moment().tz('America/Bogota');
-    await this.gestorHorasFichaService.actualizarHorasInstructor({
-      reporte: reporteFichas,
-      year: now.year(),
-      id_instructor: evento.instructor,
-    });
-
-    const busquedaInstructor = evento.eventos.map((ev) => ({
-      mes: ev.mes,
-      year: ev.year,
-      instructor: evento.instructor,
-    }));
-    const registrosEventoExistente = await this.eventoModel.find({
-      $or: busquedaInstructor,
-    });
-
-    const eventosCrear = [];
-    if (registrosEventoExistente.length === 0) {
-      //[{mes:1, year:2002, instructor:12h2h1v223tsvhg347hb, eventos:[] }]
-
-      evento.eventos.forEach((ev) => {
-        const indexMes = eventosCrear.findIndex((e) => e.mes == ev.mes);
-        const copia_e = { ...ev };
-        delete copia_e.mes;
-        delete copia_e.year;
-        indexMes > -1
-          ? eventosCrear[indexMes].eventos.push(copia_e)
-          : eventosCrear.push({
-              mes: ev.mes,
-              year: ev.year,
-              instructor: evento.instructor,
-              eventos: [copia_e],
-            });
+      const now = moment().tz('America/Bogota');
+      await this.gestorHorasFichaService.actualizarHorasInstructor({
+        reporte: reporteFichas,
+        year: now.year(),
+        id_instructor: evento.instructor,
       });
-    } else {
-      //Hay un evento que ya tiene documento, pero puede que hayan eventos que no tengan documentos creados
-      //Filtro para guardar donde corresponda
-      evento.eventos.forEach((ev) => {
-        const indexEvento = registrosEventoExistente.findIndex(
-          (reg) => reg.mes == ev.mes && reg.year == ev.year,
-        );
-        const copia_e = { ...ev };
-        delete copia_e.mes;
-        delete copia_e.year;
-        //Existe, toca actualizar
-        if (indexEvento > -1) {
-          registrosEventoExistente[indexEvento].eventos.push(copia_e);
-        } else {
-          const indexEventoCrear = eventosCrear.findIndex(
-            (ev_crear) => ev_crear.mes == ev.mes && ev_crear.year == ev.year,
-          );
-          indexEventoCrear > -1
-            ? eventosCrear[indexEventoCrear].eventos.push(copia_e)
+
+      const busquedaInstructor = evento.eventos.map((ev) => ({
+        mes: ev.mes,
+        year: ev.year,
+        instructor: evento.instructor,
+      }));
+      registrosEventoExistente = await this.eventoModel.find({
+        $or: busquedaInstructor,
+      });
+
+      eventosCrear = [];
+      if (registrosEventoExistente.length === 0) {
+        //[{mes:1, year:2002, instructor:12h2h1v223tsvhg347hb, eventos:[] }]
+
+        evento.eventos.forEach((ev) => {
+          const indexMes = eventosCrear.findIndex((e) => e.mes == ev.mes);
+          const copia_e = { ...ev };
+          delete copia_e.mes;
+          delete copia_e.year;
+          indexMes > -1
+            ? eventosCrear[indexMes].eventos.push(copia_e)
             : eventosCrear.push({
                 mes: ev.mes,
                 year: ev.year,
                 instructor: evento.instructor,
                 eventos: [copia_e],
               });
-        }
-      });
-    }
-    //Colecciones a crear
-    if (eventosCrear.length > 0) {
-      await this.eventoModel.insertMany(eventosCrear);
-    }
+        });
+      } else {
+        //Hay un evento que ya tiene documento, pero puede que hayan eventos que no tengan documentos creados
+        //Filtro para guardar donde corresponda
+        evento.eventos.forEach((ev) => {
+          const indexEvento = registrosEventoExistente.findIndex(
+            (reg) => reg.mes == ev.mes && reg.year == ev.year,
+          );
+          const copia_e = { ...ev };
+          delete copia_e.mes;
+          delete copia_e.year;
+          //Existe, toca actualizar
+          if (indexEvento > -1) {
+            registrosEventoExistente[indexEvento].eventos.push(copia_e);
+          } else {
+            const indexEventoCrear = eventosCrear.findIndex(
+              (ev_crear) => ev_crear.mes == ev.mes && ev_crear.year == ev.year,
+            );
+            indexEventoCrear > -1
+              ? eventosCrear[indexEventoCrear].eventos.push(copia_e)
+              : eventosCrear.push({
+                  mes: ev.mes,
+                  year: ev.year,
+                  instructor: evento.instructor,
+                  eventos: [copia_e],
+                });
+          }
+        });
+      }
+      //Colecciones a crear
+      if (eventosCrear.length > 0) {
+        await this.eventoModel.insertMany(eventosCrear);
+      }
 
-    //Colecciones a actualizar
-    if (registrosEventoExistente.length > 0) {
-      for await (const ev of registrosEventoExistente) {
-        await this.eventoModel.findByIdAndUpdate(ev._id, ev).exec();
+      //Colecciones a actualizar
+      if (registrosEventoExistente.length > 0) {
+        for await (const ev of registrosEventoExistente) {
+          await this.eventoModel.findByIdAndUpdate(ev._id, ev).exec();
+        }
       }
     }
+    //Retorno los eventos en conflicto
+    if (mensajeConflict.length > 0) {
+      throw new ConflictException(mensajeConflict);
+    }
+
     return {
       isUpdated: registrosEventoExistente.length > 0,
       statusCode: HttpStatus.OK,
